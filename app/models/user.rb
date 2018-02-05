a0547e131d81b8458c4a30fd809497c042b16c5a
Ed <ea.litten@gmail.com>
2018-02-05 15:55:10 +0000

    Make username & email required

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User
   include DataMapper::Resource
 
   property :id, Serial
-  property :username, String, required: true
-  property :email, String, required: true
+  property :username, String, required: true, unique: true
+  property :email, String, required: true, format: :email_address, unique: true
 
 end
