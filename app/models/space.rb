ff58724d8f0771f5e4bad1d4d8699300bc37681a
Ed <ea.litten@gmail.com>
2018-02-07 16:58:20 +0000

    Add availability

--- app/models/space.rb
+++ app/models/space.rb
@@ -3,12 +3,12 @@ class Space
   include DataMapper::Resource
 
   property :id, Serial
-  property :title, String
-  # required: true
-  property :description, String
-  # , required: true
-  property :price, Integer
-  # , required: true
+  property :title, String, required: true
+  property :description, String, required: true
+  property :price, Integer, required: true
+  property :availability_start, Date
+  property :availability_end, Date
+
   belongs_to :user
 
 end
