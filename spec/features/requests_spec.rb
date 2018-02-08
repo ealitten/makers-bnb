aed83ce28a92cf0eeb08bba84873873b7a73f6dd
Ed <ea.litten@gmail.com>
2018-02-08 16:47:39 +0000

    Also show approved requests on requests page (in separate list, sans buttons)
    Co-authored-by: Jennifer <jennifer.elaine.mairead.shepherd@gmail.com>

--- spec/features/requests_spec.rb
+++ spec/features/requests_spec.rb
@@ -10,8 +10,10 @@ feature 'user can view requests' do
     click_button("Sign out")
     login(name: "Owner")
     visit '/requests'
-    expect(page).to have_content("You have requests waiting:")
-    expect(page).to have_content("2109-01-02")
+    within 'ul#new_requests' do
+      expect(page).to have_content("You have requests waiting:")
+      expect(page).to have_content("2109-01-02")
+    end
   end
 
   scenario 'user has no requests if no listings' do
@@ -42,8 +44,6 @@ feature 'user can view requests' do
     expect(page).not_to have_content("You have requests waiting:")
   end
 
-
-
   scenario 'denied requests are no longer shown on the requests page' do
     sign_up(name: 'Owner', email: 'user1@example.com')
     list_space
@@ -58,6 +58,24 @@ feature 'user can view requests' do
     click_button 'Deny'
     expect(page).not_to have_content("You have requests waiting:")
     expect(page).not_to have_content("2109-01-02")
+  end
+
+  scenario 'approved requests are still shown on the requests page' do
+    sign_up(name: 'Owner', email: 'user1@example.com')
+    list_space
+    click_button("Sign out")
+    sign_up(name: 'Holidaymaker', email: 'user2@example.com')
+    visit '/spaces'
+    fill_in :date, with: '02/01/2109'
+    click_button 'Hire'
+    click_button("Sign out")
+    login(name: "Owner")
+    visit '/requests'
+    click_button 'Approve'
+    within 'ul#approved_requests' do
+      expect(page).to have_content("Approved requests:")
+      expect(page).to have_content("2109-01-02")
+    end
   end
 
 end
