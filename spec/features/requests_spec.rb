b8090c57f576a9c681cba88ef0c0445a348170d7
Ed <ea.litten@gmail.com>
2018-02-08 16:26:08 +0000

    user can only see requests for their own spaces

--- spec/features/requests_spec.rb
+++ spec/features/requests_spec.rb
@@ -27,6 +27,20 @@ feature 'user can view requests' do
     expect(page).not_to have_content("You have requests waiting:")
     expect(page).not_to have_content("2109-01-02")
   end
+
+  scenario 'user can only view requests for their own spaces' do
+    sign_up(name: 'Owner', email: 'user1@example.com')
+    list_space
+    click_button("Sign out")
+    sign_up(name: 'Holidaymaker', email: 'user2@example.com')
+    visit '/spaces'
+    fill_in :date, with: '02/01/2109'
+    click_button 'Hire'
+    click_button("Sign out")
+    sign_up(name: 'Random 3rd party', email: 'user3@example.com')
+    visit '/requests'
+    expect(page).not_to have_content("You have requests waiting:")
+  end
 end
 
 feature 'user can accept and deny requests' do
